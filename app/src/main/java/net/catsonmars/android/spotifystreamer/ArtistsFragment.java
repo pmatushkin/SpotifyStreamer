fbb47ed59cbd4625577c1322c9684f7cbda72093
pmatushkin <pmatushkin@gmail.com>
2015-06-29 21:26:11 -0400

    Add main activity layout, and dummy data

---
+++ app/src/main/java/net/catsonmars/android/spotifystreamer/ArtistsFragment.java
@@ -0,0 +1,66 @@
+package net.catsonmars.android.spotifystreamer;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ArtistsFragment extends Fragment {
+
+    public ArtistsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Create some dummy data for the ListView.  Here's a sample weekly forecast
+        String[] data = {
+                "Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"
+        };
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+
+        ArrayAdapter<String> mArtistsAdapter;
+        mArtistsAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_artist,
+                R.id.txtArtist,
+                weekForecast);
+
+        View rootView = inflater.inflate(R.layout.fragment_artists, container, false);
+
+        ListView listView = (ListView)rootView.findViewById(R.id.lvArtists);
+        listView.setAdapter(mArtistsAdapter);
+
+        return rootView;
+    }
+}

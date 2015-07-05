7549343c4292c491dfc463ca068e2a7a1cbb177e
pmatushkin <pmatushkin@gmail.com>
2015-07-05 14:13:32 -0400

    Retain the results of the artist search on device rotation

--- app/src/main/java/net/catsonmars/android/spotifystreamer/ArtistsFragment.java
+++ app/src/main/java/net/catsonmars/android/spotifystreamer/ArtistsFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,20 +24,36 @@ import kaaes.spotify.webapi.android.models.ArtistsPager;
 
 
 /**
- * A placeholder fragment containing a simple view.
+ * The code for retaining the fragment state was adopted from http://www.androiddesignpatterns.com/2013/04/retaining-objects-across-config-changes.html
  */
 public class ArtistsFragment extends Fragment {
+    String LOG_TAG = "";
 
     private ArtistsAdapter mArtistsAdapter;
+    private String mSearchArgument;
+    private ArrayList<Artist> mSpotifyArtists;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Retain this fragment across configuration changes.
+        setRetainInstance(true);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        LOG_TAG = getString(R.string.app_log_tag);
+
+        if (mSpotifyArtists == null)
+            mSpotifyArtists = new ArrayList<Artist>();
+
         mArtistsAdapter = new ArtistsAdapter(
                 getActivity(),
                 R.layout.list_item_artist,
                 R.id.txtArtistName,
-                new ArrayList<Artist>());
+                mSpotifyArtists);
 
         View rootView = inflater.inflate(R.layout.fragment_artists, container, false);
 
@@ -64,54 +81,68 @@ public class ArtistsFragment extends Fragment {
             public void onTextChanged(CharSequence s, int start, int before, int count) {  }
 
             @Override
-            public void afterTextChanged(Editable s) {
-                updateArtists(s.toString());
-            }
+            public void afterTextChanged(Editable s) { updateArtists(s.toString()); }
         });
 
         return rootView;
     }
 
+    /*
+    Compare the saved search argument and the current search argument;
+    Do the search only if the arguments don't match
+
+    The deletion of the last character in the search box doesn't trigger the search because of the second 'if'.
+    I didn't intend to implement this behaviour but I like it better than blanking the list view when the user deletes the last character.
+     */
     private void updateArtists(String searchArgument) {
-        if (searchArgument.isEmpty()) {
-            /* Removed this toast, because it pops up too often, cluttering the screen */
-            /*
-            Toast toast = Toast.makeText(getActivity(), getString(R.string.warn_empty_search_term), Toast.LENGTH_SHORT);
-            toast.show();
-            */
-        } else {
-            FetchArtistsTask task = new FetchArtistsTask();
-            task.execute(searchArgument);
+        if (!searchArgument.equals(this.mSearchArgument)) {
+            this.mSearchArgument = searchArgument;
+
+            if (!searchArgument.isEmpty()) {
+                FetchArtistsTask task = new FetchArtistsTask();
+                task.execute(searchArgument);
+            }
         }
     }
 
     public class FetchArtistsTask extends AsyncTask<String, Void, ArrayList<Artist>> {
         @Override
         protected ArrayList<Artist> doInBackground(String... params) {
-            ArrayList<Artist> spotifyArtists = new ArrayList<Artist>();
+            mSpotifyArtists = new ArrayList<Artist>();
 
-            SpotifyApi api = new SpotifyApi();
-            SpotifyService spotify = api.getService();
-            ArtistsPager artists = spotify.searchArtists(params[0]);
+            try {
+                SpotifyApi api = new SpotifyApi();
+                SpotifyService spotify = api.getService();
+                ArtistsPager artists = spotify.searchArtists(params[0]);
 
-            for(Artist artist : artists.artists.items) {
-                spotifyArtists.add(artist);
+                for (Artist artist : artists.artists.items) {
+                    mSpotifyArtists.add(artist);
+                }
+            }
+            catch (Exception e) {
+                Log.e(LOG_TAG, e.getMessage());
+                mSpotifyArtists = null;
             }
 
-            return spotifyArtists;
+            return mSpotifyArtists;
         }
 
         @Override
         protected void onPostExecute(ArrayList<Artist> artists) {
-            mArtistsAdapter.clear();
-
-            for(Artist artist : artists) {
-                mArtistsAdapter.add(artist);
-            }
-
-            if (mArtistsAdapter.isEmpty()) {
-                Toast toast = Toast.makeText(getActivity(), getString(R.string.warn_empty_search_artists), Toast.LENGTH_SHORT);
+            if (artists == null) {
+                Toast toast = Toast.makeText(getActivity(), getString(R.string.error_search_artists), Toast.LENGTH_SHORT);
                 toast.show();
+            } else {
+                mArtistsAdapter.clear();
+
+                for (Artist artist : artists) {
+                    mArtistsAdapter.add(artist);
+                }
+
+                if (mArtistsAdapter.isEmpty()) {
+                    Toast toast = Toast.makeText(getActivity(), getString(R.string.warn_empty_search_artists), Toast.LENGTH_SHORT);
+                    toast.show();
+                }
             }
         }
     }
--- app/src/main/java/net/catsonmars/android/spotifystreamer/MainActivity.java
+++ app/src/main/java/net/catsonmars/android/spotifystreamer/MainActivity.java
@@ -1,19 +1,36 @@
 package net.catsonmars.android.spotifystreamer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
+/**
+ * The code for retaining the fragment state was adopted from http://www.androiddesignpatterns.com/2013/04/retaining-objects-across-config-changes.html
+ */
 public class MainActivity extends ActionBarActivity {
+
+    private static final String TAG_FRAGMENT = "artists_fragment";
+
+    private ArtistsFragment mArtistsFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        FragmentManager fm = getSupportFragmentManager();
+        mArtistsFragment = (ArtistsFragment)fm.findFragmentByTag(TAG_FRAGMENT);
+
+        // If the Fragment is non-null, then it is currently being
+        // retained across a configuration change.
+        if (mArtistsFragment == null) {
+            mArtistsFragment = new ArtistsFragment();
+            fm.beginTransaction().add(mArtistsFragment, TAG_FRAGMENT).commit();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
